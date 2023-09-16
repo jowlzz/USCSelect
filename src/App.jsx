037b88f0eec74079e5352a248698462e922e5867
jowlzz <131784168+jowlzz@users.noreply.github.com>
2023-09-16 14:04:44 +0800

    [FEAT][Julls]
    added node modules and some frontend.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='logoSpace'>
+        <center><img src="./images/uscLogo.png" />
+                <h5>Are you ready to vote?</h5>
+
+                <button>VOTE NOW!</button>
+
+        </center>
+            
+          
+        </div>
     </>
   )
 }
