eb986b28d0d2053cdcd88e80f59f0843c04f6a30
jowlzz <131784168+jowlzz@users.noreply.github.com>
2023-09-24 14:00:38 +0800

    Modified the config file for DB

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"
+// import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyASqDURLGod9tiXJbmNXlICWf-c_XdIzB4",
   authDomain: "e-vote-ph.firebaseapp.com",
@@ -16,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-W7J4L4HN0W"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+export const Auth = getAuth(app);
