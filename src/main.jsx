5353a62ea1fec9cdfdfb9eff0071e671a30d2261
Beeecc <veparojinog@gmail.com>
2023-09-24 22:14:39 +0800

    Initialized Firebase Hosting

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import LoginPage from './pages/Login.jsx'
 import RegisterAccount from './pages/Registration.jsx'
 import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import WelcomePg from './pages/WelcomePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <div>
     <Routes>
+      <Route path = '/' element = {<WelcomePg />}/>
       <Route path = '/login' element = {<LoginPage />}/>
       <Route path = '/register' element = {<RegisterAccount />}/>
       // ADD ADDITIONAL PATHS BELOW
