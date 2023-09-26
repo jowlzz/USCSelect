4f9fd2b4b19bd1f898a81e09bd1806f79f13c72e
jowlzz <131784168+jowlzz@users.noreply.github.com>
2023-09-26 11:30:10 +0800

    Modified user registration to admin

--- src/pages/AdminRegistration.jsx
+++ src/pages/AdminRegistration.jsx
@@ -17,6 +17,7 @@ import {
   Link,
 } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { NavLink, Link as ReactRouterLink } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { Auth } from '../firebase/config';
 
@@ -24,32 +25,51 @@ function RegisterAccount() {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showIdNumberForm, setShowIdNumberForm] = useState(false);
+  const [idNumber, setIdNumber] = useState('');
 
   const handleInputChange = (e) => {
     if (e.target.id === 'email') {
       setEmail(e.target.value);
     } else if (e.target.id === 'password') {
       setPassword(e.target.value);
+    } else if (e.target.id === 'idNumber') {
+      setIdNumber(e.target.value);
     }
   };
 
-  const handleSignUp = async (e) => {
-    e.preventDefault();
+  const handleGetCode = () => {
+    
+    if (email && password) {
+      setShowIdNumberForm(true);
+    } else {
+    
+      console.log('Please fill in both email and password fields.');
+    }
+  };
 
+  const handleSignUp = async () => {
+   
     try {
-
       const auth = getAuth();
       await createUserWithEmailAndPassword(Auth, email, password);
 
-  
-      console.log('User registered successfully.');
+ 
+      console.log('User registered successfully with idNumber:', idNumber);
 
+     
       setEmail('');
       setPassword('');
+      setIdNumber('');
+      setShowIdNumberForm(false); 
     } catch (error) {
       console.error('Error registering user: ', error.message);
     }
   };
+
+  if ((!email || !password) && showIdNumberForm) {
+    setShowIdNumberForm(false);
+  }
 
   return (
     <ChakraProvider>
@@ -61,7 +81,7 @@ function RegisterAccount() {
         <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
           <Stack align={'center'}>
             <Heading fontSize={'4xl'} textAlign={'center'}>
-              Register As Voter
+              Register As Admin
             </Heading>
           </Stack>
           <Box
@@ -69,21 +89,10 @@ function RegisterAccount() {
             bg={useColorModeValue('white', 'gray.700')}
             boxShadow={'lg'}
             p={8}>
-            <form onSubmit={handleSignUp}>
+            <form onSubmit={(e) => e.preventDefault()}>
               <Stack spacing={4}>
                 <HStack>
-                  <Box>
-                    <FormControl id="name">
-                      <FormLabel>Name</FormLabel>
-                      <Input type="text" />
-                    </FormControl>
-                  </Box>
-                  <Box>
-                    <FormControl id="idNumber">
-                      <FormLabel>ID number</FormLabel>
-                      <Input type="number" />
-                    </FormControl>
-                  </Box>
+                  <Box></Box>
                 </HStack>
                 <FormControl id="email" isRequired>
                   <FormLabel>Email address</FormLabel>
@@ -114,24 +123,52 @@ function RegisterAccount() {
                     </InputRightElement>
                   </InputGroup>
                 </FormControl>
+
+                {showIdNumberForm && (
+                  <FormControl id="idNumber" isRequired>
+                    <FormLabel>Admin Code</FormLabel>
+                    <Input
+                      type="number"
+                      id="idNumber"
+                      value={idNumber}
+                      onChange={handleInputChange}
+                    />
+                  </FormControl>
+                )}
+
                 <Stack spacing={10} pt={2}>
                   <Button
-                    type="submit"
-                    loadingText="Submitting"
+                    onClick={handleGetCode}
                     size="lg"
                     bg={'blue.400'}
                     color={'white'}
                     _hover={{
                       bg: 'blue.500',
-                    }}>
-                    Sign up
+                    }}
+                    isDisabled={!email || !password}>
+                    Get Code
                   </Button>
+                  {showIdNumberForm && (
+                    <Button
+                      onClick={handleSignUp}
+                      size="lg"
+                      bg={'green.400'}
+                      color={'white'}
+                      _hover={{
+                        bg: 'green.500',
+                      }}>
+                      Sign Up
+                    </Button>
+                  )}
                 </Stack>
               </Stack>
             </form>
             <Stack pt={6}>
               <Text align={'center'}>
-                Already a user? <Link color={'blue.400'}>Login</Link>
+                Already a user?{' '}
+                <Link color={'blue.400'} as={ReactRouterLink} to="/adminLogin">
+                  Login
+                </Link>
               </Text>
             </Stack>
           </Box>
