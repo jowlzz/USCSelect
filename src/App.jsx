54e10c9e4fbe2eafd1e6d596e41bfe2105fb6117
Vic <89066365+Beeecc@users.noreply.github.com>
2023-09-30 18:23:29 +0800

    Revert "Candidates page"
    This reverts commit 6bb0687d0ff598a38e349b76d529068bfc078dcc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { useState } from "react";
 import WelcomePg from "./pages/WelcomePage";
 
@@ -6,28 +5,6 @@ function App() {
   const [count, setCount] = useState(0);
 
   return <>{/* <WelcomePg/> */}</>;
-=======
-import { CSSReset, ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Candidates from "./pages/Candidates";
-import LoginPage from "./pages/Login";
-import RegisterAccount from "./pages/Registration";
-
-function App() {
-	return (
-		<ChakraProvider>
-			<CSSReset />
-
-			<BrowserRouter>
-				<Routes>
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterAccount />} />
-					<Route path="/candidates" element={<Candidates />} />
-				</Routes>
-			</BrowserRouter>
-		</ChakraProvider>
-	);
->>>>>>> Stashed changes
 }
 
 export default App;
