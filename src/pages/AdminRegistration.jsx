31ba13f657f97ed9e7b2e949d30fb1a464ddf630
jowlzz <131784168+jowlzz@users.noreply.github.com>
2023-10-01 11:49:54 +0800

    Made the partial FrontEnd(AdminRegis)

--- src/pages/AdminRegistration.jsx
+++ src/pages/AdminRegistration.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Flex,
   Box,
@@ -15,24 +15,33 @@ import {
   useColorModeValue,
   ChakraProvider,
   Link,
-} from "@chakra-ui/react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { NavLink, Link as ReactRouterLink } from "react-router-dom";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Auth } from "../firebase/config";
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { NavLink, Link as ReactRouterLink } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { Auth } from '../firebase/config';
 
 function RegisterAccount() {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [showIdNumberForm, setShowIdNumberForm] = useState(false);
   const [idNumber, setIdNumber] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [verificationCode, setVerificationCode] = useState('');
+  const [codeSent, setCodeSent] = useState(false);
+  const [enteredCode, setEnteredCode] = useState('');
 
   const handleInputChange = (e) => {
-    if (e.target.id === "email") {
+    if (e.target.id === 'email') {
       setEmail(e.target.value);
-    } else if (e.target.id === "password") {
+    } else if (e.target.id === 'password') {
       setPassword(e.target.value);
+      checkPasswordMatch();
+    } else if (e.target.id === 'confirmPassword') {
+      setConfirmPassword(e.target.value);
+      checkPasswordMatch();
     } else if (e.target.id === 'idNumber') {
       setIdNumber(e.target.value);
     } else if (e.target.id === 'verificationCode') {
@@ -40,13 +49,33 @@ function RegisterAccount() {
     }
   };
 
-  const handleGetCode = () => {
-    
-    if (email && password) {
-      setShowIdNumberForm(true);
+  const generateRandomCode = () => {
+    return Math.floor(100000 + Math.random() * 900000);
+  };
+
+  const handleGetCode = async () => {
+    if (email && password && passwordsMatch) {
+      const newVerificationCode = generateRandomCode();
+
+      const mailOptions = {
+        from: '', 
+        to: email,
+        subject: 'Verification Code',
+        text: `Your admin verification code is: ${newVerificationCode}`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error sending email: ', error);
+        } else {
+          console.log('Email sent: ' + info.response);
+
+          setVerificationCode(newVerificationCode);
+          setCodeSent(true);
+        }
+      });
     } else {
-    
-      console.log('Please fill in both email and password fields.');
+      console.log('Please fill in both email, password, and confirm password fields.');
     }
   };
 
@@ -56,16 +85,20 @@ function RegisterAccount() {
         const auth = getAuth();
         await createUserWithEmailAndPassword(Auth, email, password);
 
- 
-      console.log('User registered successfully with idNumber:', idNumber);
+        console.log('User registered successfully with idNumber:', idNumber);
 
-     
-      setEmail('');
-      setPassword('');
-      setIdNumber('');
-      setShowIdNumberForm(false); 
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+        setIdNumber('');
+        setVerificationCode('');
+        setShowIdNumberForm(false);
+        setEnteredCode('');
+      } else {
+        console.log('Incorrect verification code.');
+      }
     } catch (error) {
-      console.error("Error registering user: ", error.message);
+      console.error('Error registering user: ', error.message);
     }
   };
 
@@ -83,106 +116,149 @@ function RegisterAccount() {
 
   return (
     <ChakraProvider>
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'} textAlign={'center'}>
-              Register As Admin
-            </Heading>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <Stack spacing={4}>
-                <HStack>
-                  <Box></Box>
-                </HStack>
-                <FormControl id="email" isRequired>
-                  <FormLabel>Email address</FormLabel>
-                  <Input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={handleInputChange}
-                  />
-                </FormControl>
-                <FormControl id="password" isRequired>
-                  <FormLabel>Password</FormLabel>
-                  <InputGroup>
-                    <Input
-                      type={showPassword ? 'text' : 'password'}
-                      id="password"
-                      value={password}
-                      onChange={handleInputChange}
-                    />
-                    <InputRightElement h={'full'}>
-                      <Button
-                        variant={'ghost'}
-                        onClick={() =>
-                          setShowPassword((showPassword) => !showPassword)
-                        }>
-                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                </FormControl>
+      <Flex height="100vh" maxW="1500px">
+       
+        <Box
+          flex="1"
+          bg="green.500"
+          display={['none', 'block']} 
+        >
 
-                {showIdNumberForm && (
-                  <FormControl id="idNumber" isRequired>
-                    <FormLabel>Admin Code</FormLabel>
+        {/*Butang carousel image diri*/}
+
+        </Box>
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="flex-end"  
+            height="100vh"
+            p={{ base: 4, md: 8, lg: 12, xl: 16 }}
+            bg={useColorModeValue('gray.50', 'gray.800')}
+        >
+          <Stack spacing={5}>
+            <Stack align={'center'}>
+              <Heading fontSize={'4xl'} textAlign={'center'}>
+                Register As Admin
+              </Heading>
+            </Stack>
+            <Box
+              rounded={'lg'}
+              bg={useColorModeValue('white', 'gray.700')}
+              p={8}
+            >
+              <form onSubmit={(e) => e.preventDefault()}>
+                <Stack spacing={2}>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
                     <Input
-                      type="number"
-                      id="idNumber"
-                      value={idNumber}
+                      type="email"
+                      id="email"
+                      value={email}
                       onChange={handleInputChange}
                     />
                   </FormControl>
-                )}
-
-                <Stack spacing={10} pt={2}>
-                  <Button
-                    onClick={handleGetCode}
-                    size="lg"
-                    bg={'blue.400'}
-                    color={'white'}
-                    _hover={{
-                      bg: 'blue.500',
-                    }}
-                    isDisabled={!email || !password}>
-                    Get Code
-                  </Button>
-                  {showIdNumberForm && (
-                    <Button
-                      onClick={handleSignUp}
-                      size="lg"
-                      bg={'green.400'}
-                      color={'white'}
-                      _hover={{
-                        bg: 'green.500',
-                      }}>
-                      Sign Up
-                    </Button>
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        id="password"
+                        value={password}
+                        onChange={handleInputChange}
+                        onBlur={checkPasswordMatch}
+                      />
+                      <InputRightElement h={'full'}>
+                        <Button
+                          variant={'ghost'}
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        >
+                          {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id="confirmPassword" isRequired>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type="password"
+                        id="confirmPassword"
+                        value={confirmPassword}
+                        onChange={handleInputChange}
+                        onBlur={checkPasswordMatch}
+                      />
+                    </InputGroup>
+                  </FormControl>
+                  {!passwordsMatch && (
+                    <Text color="red.500">Passwords do not match.</Text>
                   )}
+                  {showIdNumberForm && (
+                    <FormControl id="idNumber" isRequired>
+                      <FormLabel>Admin Code</FormLabel>
+                      <Input
+                        type="number"
+                        id="idNumber"
+                        value={idNumber}
+                        onChange={handleInputChange}
+                      />
+                    </FormControl>
+                  )}
+                  {codeSent && (
+                    <FormControl>
+                      <FormLabel>Verification Code</FormLabel>
+                      <Input
+                        type="text"
+                        id="verificationCode"
+                        value={enteredCode}
+                        onChange={handleInputChange}
+                      />
+                    </FormControl>
+                  )}
+                  <Stack spacing={10} pt={2}>
+                    {!codeSent && (
+                      <Button
+                        onClick={handleGetCode}
+                        size="lg"
+                        bg={'blue.400'}
+                        color={'white'}
+                        _hover={{
+                          bg: 'blue.500',
+                        }}
+                        isDisabled={!areFieldsValid()}
+                      >
+                        Get Code
+                      </Button>
+                    )}
+                    {codeSent && (
+                      <Button
+                        onClick={handleSignUp}
+                        size="lg"
+                        bg={'green.400'}
+                        color={'white'}
+                        _hover={{
+                          bg: 'green.500',
+                        }}
+                        isDisabled={!enteredCode || enteredCode !== verificationCode}
+                      >
+                        Sign Up
+                      </Button>
+                    )}
+                  </Stack>
                 </Stack>
+              </form>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Already a user?{' '}
+                  <Link color={'blue.400'} as={ReactRouterLink} to="/adminLogin">
+                    Login
+                  </Link>
+                </Text>
               </Stack>
-            </form>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user?{' '}
-                <Link color={'blue.400'} as={ReactRouterLink} to="/adminLogin">
-                  Login
-                </Link>
-              </Text>
-            </Stack>
-          </Box>
-        </Stack>
+            </Box>
+          </Stack>
+        </Box>
       </Flex>
     </ChakraProvider>
   );
